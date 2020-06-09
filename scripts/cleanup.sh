0649c3d3c9361aa794fa585974d9677360519e5a
Ben Sykes <hootieben@gmail.com>
2020-06-09 18:28:36 -0500

    Initial commit

---
+++ scripts/cleanup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -eux
+
+# Uninstall Ansible and remove PPA.
+apt -y remove --purge ansible
+apt-add-repository --remove ppa:ansible/ansible
+
+# Apt cleanup.
+apt autoremove
+apt update
+
+rm -rf /home/vagrant/.ansible
